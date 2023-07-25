10e0b690da90e77aba0bff0e1b5e8843e4b49966
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-07-25 13:33:47 +0400

    add TransportMode.h & Transport.h

--- lib/Transport.h
+++ lib/Transport.h
@@ -1,10 +1,42 @@
 #ifndef COMPANY_TRANSPORT_H
 #define COMPANY_TRANSPORT_H
 
+#include <string>
+#include <vector>
+
+#include "TransportMode.h"
+
+using namespace std;
 
 class Transport {
+private:
+    string _model;
+    double _max_weight;
+    string _type;
+    TransportMode _mode;
+public:
+    Transport(const string &model, double maxWeight, const string &type, TransportMode mode) : _model(model),
+                                                                                               _max_weight(maxWeight),
+                                                                                               _type(type),
+                                                                                               _mode(mode) {}
 
+    double GetMaxWeight() const {
+        return _max_weight;
+    }
+
+    const string &GetType() const {
+        return _type;
+    }
+
+    void SetMaxWeight(double maxWeight) {
+        _max_weight = maxWeight;
+    }
+
+    void SetType(const string &type) {
+        _type = type;
+    }
 };
+
 
 
 #endif
---
+++ lib/TransportMode.h
@@ -0,0 +1,8 @@
+#ifndef COMPANY_TRANSPORTMODE_H
+#define COMPANY_TRANSPORTMODE_H
+
+enum TransportMode {
+    PASSENGER,CARGO
+};
+
+#endif
